5de9d846615dd856f0dd0cc274cfbce1612efcae
midnightsun183 <midnightsun183@users.noreply.github.com>
2018-06-07 05:29:46 -0400

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,73 @@
+function generateInsult() {
+
+var Pronoun = [
+    'You',
+    'That',
+    'What a'
+];
+
+var Adjective1 = [
+    ' vicious',
+    ' rancid',
+    ' savage',
+    ' fiendish',
+    ' filthy',
+    ' rotten',
+    ' treacherous',
+    ' heinous',
+    ' scurvy',
+    ' devious',
+    ' grisly',
+    ' hamwit',
+    ' filthy',
+    ' foetid',
+    ' awful',
+    ' hateful',
+    ' hellish',
+    ' foul',
+    ' bilious',
+    ' hideous'
+];
+
+var Adjective2 = [
+    ' festering',
+    ' stinking',
+    ' crazed',
+    ' deranged',
+    ' demented',
+    ' soul-ripping',
+    ' drooling',
+    ' rabbit-punching',
+    ' knee-crawling',
+    ' thieving',
+    ' cheapjack',
+    ' twisted',
+    ' backwoods',
+    ' diseased',
+    ' weird',
+    ' black-hearted',
+    ' incurably violent'
+];
+
+var Noun = [
+    ' bastard',
+    ' swine',
+    ' pig farmer',
+    ' pile of rhino warts',
+    ' used car salesman',
+    ' nazi',
+    ' geek'
+];
+
+var randomPronoun = Math.floor(Math.random() * Pronoun.length);
+
+var randomAdjective1 = Math.floor(Math.random() * Adjective1.length);
+
+var randomAdjective2 = Math.floor(Math.random() * Adjective2.length);
+
+var randomNoun = Math.floor(Math.random() * Noun.length);
+
+var insult = Pronoun[randomPronoun].concat(Adjective1[randomAdjective1], Adjective2[randomAdjective2], Noun[randomNoun]);
+
+document.getElementById("insult-space").innerText = insult;
+}
