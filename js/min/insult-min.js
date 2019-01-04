734d5c686251c201c15a3f37507aaa2047637770
midnightsun183 <midnightsun183@users.noreply.github.com>
2019-01-04 11:13:22 -0500

    Update insult-min.js

--- js/min/insult-min.js
+++ js/min/insult-min.js
@@ -1,4 +1,6 @@
-!function(a){function s(a,s,n){var t=a[Math.floor(Math.random()*a.length)],e=s[Math.floor(Math.random()*s.length)],l=n[Math.floor(Math.random()*n.length)];return[t,e,l]}function n(){var n=a(".fucking.description .sentence");e=s(o,r,p),l=e[2],a(".insult h1.ass").html(e[0]+" "+e[1]),n.html(e[2]),n.find("span.ass").html(e[0]+" "+e[1]),n.find("span.aan").html(t(e[0]))}function t(a){return"a"===a.toLowerCase().charAt(0)?"an":"a"}var e="",l="",o=["$MMM",
+!function(a){function s(a,s,n){var t=a[Math.floor(Math.random()*a.length)],e=s[Math.floor(Math.random()*s.length)],l=n[Math.floor(Math.random()*n.length)];
+return[t,e,l]}function n(){var n=a(".fucking.description .sentence");e=s(o,r,p),l=e[2],a(".insult h1.ass").html(e[0]+" "+e[1]),n.html(e[2]),n.find("span.ass").html(e[0]+" "+e[1]),n.find("span.aan").html(t(e[0]))}function t(a){return"a"===a.toLowerCase().charAt(0)?"an":"a"}var e="",l="",o=[
+"$MMM",
 "$AOS",
 "$ABT",
 "$ABBV",
@@ -502,7 +504,8 @@
 "$YUM",
 "$ZBH",
 "$ZION",
-"$ZTS"],
+"$ZTS"
+],
     r=[
       "Bagel",
       "Biscuit",
@@ -539,7 +542,8 @@
       "Twiddler",
       "Waffle",
       "Wanker",
-      "Whistle"],
+      "Whistle"
+    ],
     p=[
         "That <span class='ass'></span> just cut me off!",
         "My boss is a major <span class='ass'></span>!",
@@ -563,5 +567,6 @@
         "What kind of <span class='ass'></span> slows down to merge on the highway?",
         "You've got a face like <span class='aan'></span> <span class='ass'></span>.",
         "Nothing personal, but you're a real <span class='ass'></span>.",
-        "What a bunch of <span class='ass'></span>s.","That <span class='ass'></span> is legally dead in 27 states - plus Guam."];
+        "What a bunch of <span class='ass'></span>s.","That <span class='ass'></span> is legally dead in 27 states - plus Guam."
+    ];
 a("button.damn").on("click",n),n(),a("a.twitter-share-button").on("click",function(s){var n=a(this).attr("href"),o=n.split("text="),r=l.replace("<span class='ass'></span>",e[0]+" "+e[1]);r=r.replace("<span class='aan'></span>",t(e[0]));var p=o[0]+"text="+e[0]+" "+e[1]+' (noun) "'+r+'" Get yours at';a(this).attr("href",encodeURI(p))})}(jQuery);
